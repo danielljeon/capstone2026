44a54af3b3443b7ff9e6751b7ea17cbf5b26d59c
Daniel Jeon <48095779+danielljeon@users.noreply.github.com>
2026-03-18 12:15:30 -0400

    Update motor ID script for ST3215

--- drivers/motor_id.py
+++ drivers/motor_id.py
@@ -2,20 +2,21 @@ import time
 
 import serial
 
-from motor_rsbl120 import read_position_step, set_servo_id_nvm
+from motor_rsbl120 import rsbl120_read_position_step, rsbl120_set_servo_id_nvm
+from motor_st3215 import st3215_read_position_step, st3215_set_servo_id_nvm
 from robot_arm import JointCal
 
 
-def id_motor(
+def rsbl120_id_motor(
     port: str,
     old_id: int,
     new_id: int,
     set_nvm: bool = False,
     print_pos: bool = False,
 ):
-    def __print_pos(joint_cal: JointCal):
+    def __rsbl120_print_pos(joint_cal: JointCal):
         time.sleep(1)
-        pos = read_position_step(joint_cal)
+        pos = rsbl120_read_position_step(joint_cal)
         print("Position:", pos)
 
     # New communication interface.
@@ -24,17 +25,51 @@ def id_motor(
     # Old joint check.
     old_cal = JointCal(name="old_cal", comm=comm, servo_id=old_id)
     if print_pos:
-        __print_pos(old_cal)
+        __rsbl120_print_pos(old_cal)
 
     # Update servo ID.
     if set_nvm:
-        set_servo_id_nvm(old_cal, new_id)
+        rsbl120_set_servo_id_nvm(old_cal, new_id)
         time.sleep(0.1)
 
     # New joint check.
     new_cal = JointCal(name="new_cal", comm=comm, servo_id=new_id)
     if print_pos:
-        __print_pos(new_cal)
+        __rsbl120_print_pos(new_cal)
+
+    # Close communication.
+    comm.close()
+
+
+def st3215_id_motor(
+    port: str,
+    old_id: int,
+    new_id: int,
+    set_nvm: bool = False,
+    print_pos: bool = False,
+):
+    def __st3215_print_pos(joint_cal: JointCal):
+        time.sleep(1)
+        pos = st3215_read_position_step(joint_cal)
+        print("Position:", pos)
+
+    # New communication interface.
+    comm = serial.Serial(port, 1_000_000, timeout=0.1)
+
+    # Old joint check.
+    old_cal = JointCal(name="old_cal", comm=comm, servo_id=old_id)
+    if print_pos:
+        __st3215_print_pos(old_cal)
+
+    # Update servo ID.
+    if set_nvm:
+        st3215_set_servo_id_nvm(old_cal, new_id)
+        time.sleep(0.1)
+
+    # New joint check.
+    new_cal = JointCal(name="new_cal", comm=comm, servo_id=new_id)
+    if print_pos:
+        __st3215_print_pos(new_cal)
 
     # Close communication.
     comm.close()
