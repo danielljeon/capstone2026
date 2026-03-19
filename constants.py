f71adaeab12733e700ee8a4c0ff4f96be2dd5fea
danielljeon <48095779+danielljeon@users.noreply.github.com>
2026-03-19 14:49:46 -0400

    Update constants.py for pwm_node

--- constants.py
+++ constants.py
@@ -2,6 +2,11 @@
 
 IK_FILE_CSV = ".csv"
 
+# CAN bus.
+PWM_NODE_SERVO_INTERFACE = "socketcan"
+PWM_NODE_SERVO_CHANNEL = "can0"
+PWM_NODE_SERVO_BITRATE = 500_000
+
 # Serial ports.
 RSBL120_PORT = "/dev/cu.usbmodem5ABA0052171"
 ST3215_PORT = "/dev/cu.usbmodem5A460829321"
