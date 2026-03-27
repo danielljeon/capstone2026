7ada0c09c63e3e071969354086d2d31435e988b1
danielljeon <48095779+danielljeon@users.noreply.github.com>
2026-03-26 23:38:55 -0400

    Split motor joints declarations

--- main.py
+++ main.py
@@ -1,98 +1,16 @@
 import time
 
 from constants import *
+from drivers.motor_joints import joint_cals
 from drivers.motor_rsbl120 import (
-    rsbl120_send_move,
     rsbl120_open_comm,
     rsbl120_close_comm,
 )
 from drivers.motor_st3215 import (
-    st3215_send_move,
     st3215_open_comm,
     st3215_close_comm,
 )
 from robot_arm import *
-
-joint_cals = [
-    JointCal(
-        name="st3215-12_1",
-        comm=None,
-        servo_id=2,
-        hardware_zero=0.0,
-        software_zero_rad=0.0,
-        sign=+1,
-        rad_min=-0.0,
-        rad_max=+0.0,
-        init_func=motor_init,
-        move_func=st3215_send_move,
-        deinit_func=motor_deinit,
-    ),
-    JointCal(
-        name="rsbl120-24_1",
-        comm=None,
-        servo_id=2,
-        hardware_zero=3384,
-        software_zero_rad=0.0,
-        sign=-1,
-        rad_min=-2.44346,
-        rad_max=0.0,
-        init_func=motor_init,
-        move_func=rsbl120_send_move,
-        deinit_func=motor_deinit,
-    ),
-    JointCal(
-        name="rsbl120-24_2",
-        comm=None,
-        servo_id=3,
-        hardware_zero=1056,
-        software_zero_rad=0.0,
-        sign=+1,
-        rad_min=-0.174533,
-        rad_max=3.31613,
-        init_func=motor_init,
-        move_func=rsbl120_send_move,
-        deinit_func=motor_deinit,
-    ),
-    JointCal(
-        name="rsbl120-24_3",
-        comm=None,
-        servo_id=4,
-        hardware_zero=1100,
-        software_zero_rad=0.0,
-        sign=-1,
-        rad_min=-3.31613,
-        rad_max=0.174533,
-        init_func=motor_init,
-        move_func=rsbl120_send_move,
-        deinit_func=motor_deinit,
-    ),
-    JointCal(
-        name="rsbl120-24_4",
-        comm=None,
-        servo_id=5,
-        hardware_zero=1874,
-        software_zero_rad=0.0,
-        sign=-1,
-        rad_min=0.0,
-        rad_max=+2.44346,
-        init_func=motor_init,
-        move_func=rsbl120_send_move,
-        deinit_func=motor_deinit,
-    ),
-    JointCal(
-        name="st3215-12_2",
-        comm=None,
-        servo_id=3,
-        hardware_zero=0.0,
-        software_zero_rad=0.0,
-        sign=+1,
-        rad_min=-0.0,
-        rad_max=+0.0,
-        init_func=motor_init,
-        move_func=st3215_send_move,
-        deinit_func=motor_deinit,
-    ),
-]
 
 
 def confirm_keys(task: str | None = None):
