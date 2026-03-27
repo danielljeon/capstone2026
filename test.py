6fc9268910cb048958cad121d44663d6b2ea2cce
danielljeon <48095779+danielljeon@users.noreply.github.com>
2026-03-27 01:06:06 -0400

    Update test script (3)

--- test.py
+++ test.py
@@ -2,6 +2,15 @@ import time
 
 from drivers.motor_rsbl120 import rsbl120_read_position_step
 from drivers.motor_st3215 import st3215_read_position_step
+from robot.end_effectors import (
+    EE1_TC,
+    EE2_TC,
+    EE1_TOOL,
+    EE2_TOOL,
+    lock_tool_changer,
+    release_tool_changer,
+    run_tool_end,
+)
 from robot.motor_joints import JOINTS
 from robot_arm import JointCal
 from setup import set_comms, deinit_comms
@@ -13,12 +22,10 @@ ST3215_COMM_TARGET = False  # TODO: Set to True and uncomment below.
 
 def report_q():
     def __rsbl120_print_pos(joint_cal: JointCal):
-        time.sleep(1)
         pos = rsbl120_read_position_step(joint_cal)
         print(f"{joint.name} ({joint.servo_id}) position:", pos)
 
     def __st3215_print_pos(joint_cal: JointCal):
-        time.sleep(1)
         pos = st3215_read_position_step(joint_cal)
         print(f"{joint.name} ({joint.servo_id}) position:", pos)
 
@@ -52,30 +59,20 @@ def main():
             st3215_comm_target=ST3215_COMM_TARGET,
         )
 
-        """Single time call functions (does not require init calls)"""
-        # from robot.end_effectors import (
-        #     EE1_TC,
-        #     EE2_TC,
-        #     EE1_TOOL,
-        #     EE2_TOOL,
-        #     lock_tool_changer,
-        #     release_tool_changer,
-        #     run_tool_end,
-        # )
-        #
-        # lock_tool_changer(EE1_TC)
-        # lock_tool_changer(EE2_TC)
-        # time.sleep(1)
-        # run_tool_end(EE1_TOOL, 1, 25, True, 350.0)
-        # run_tool_end(EE2_TOOL, 1, 25, True, 350.0)
-        # time.sleep(1)
-        # run_tool_end(EE1_TOOL, 1, 25, False, 350.0)
-        # run_tool_end(EE2_TOOL, 1, 25, False, 350.0)
-        # time.sleep(1)
-        # release_tool_changer(EE1_TC)
-        # release_tool_changer(EE2_TC)
+        if CAN_BUS_TARGET:
+            lock_tool_changer(EE1_TC)
+            lock_tool_changer(EE2_TC)
+            time.sleep(1)
+            run_tool_end(EE1_TOOL, 1, 25, True, 350.0)
+            run_tool_end(EE2_TOOL, 1, 25, True, 350.0)
+            time.sleep(1)
+            run_tool_end(EE1_TOOL, 1, 25, False, 350.0)
+            run_tool_end(EE2_TOOL, 1, 25, False, 350.0)
+            time.sleep(1)
+            release_tool_changer(EE1_TC)
+            release_tool_changer(EE2_TC)
 
-        """Dynamic function calls"""
+        # TODO: uncomment.
         # for joint in JOINTS:
         #     joint.move(position_rad=0, move_time_ms=500)
 
@@ -85,4 +82,5 @@ def main():
 
 if __name__ == "__main__":
     report_q()
+    print()
     main()
