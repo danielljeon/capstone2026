d2eec3e9814e88162f86a76ad9c3e3ae896ffb88
danielljeon <48095779+danielljeon@users.noreply.github.com>
2026-03-26 21:39:01 -0400

    Add Windows COM ports

--- constants.py
+++ constants.py
@@ -8,5 +8,5 @@ PWM_NODE_SERVO_CHANNEL = "can0"
 PWM_NODE_SERVO_BITRATE = 500_000
 
 # Serial ports.
-RSBL120_PORT = "/dev/cu.usbmodem5ABA0052171"
-ST3215_PORT = "/dev/cu.usbmodem5A460829321"
+RSBL120_PORT = "COM4"  # "/dev/cu.usbmodem5ABA0052171"
+ST3215_PORT = "COM3"  # "/dev/cu.usbmodem5A460829321"
