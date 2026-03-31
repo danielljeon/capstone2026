4e1e70b33b9d128f6613645e12560c7102f46b32
danielljeon <48095779+danielljeon@users.noreply.github.com>
2026-03-31 14:51:39 -0400

    Update default COM ports

--- constants.py
+++ constants.py
@@ -8,7 +8,7 @@ PWM_NODE_SERVO_CHANNEL = "can0"
 PWM_NODE_SERVO_BITRATE = 500_000
 
 # Serial ports.
-RSBL120_PORT = "COM4"  # "/dev/cu.usbmodem5ABA0052171"
+RSBL120_PORT = "COM6"  # "/dev/cu.usbmodem5ABA0052171"
 ST3215_PORT = "COM5"  # "/dev/cu.usbmodem5ABA0050551"
 
 # IK calculation and execution frame time step (seconds).
