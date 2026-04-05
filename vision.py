d3d7ec1f7000bb5175b2a28f52a5302c20485406
danielljeon <48095779+danielljeon@users.noreply.github.com>
2026-04-05 02:44:34 -0400

    Fix vision (attempt 4)

--- vision.py
+++ vision.py
@@ -24,7 +24,7 @@ T_APRIL_TAG_TO_EE_ROT = np.array(
         [1, 0, 0, 0],
         [0, 0, 0, 1],
     ]
-)
+)  # Rx(-90) * Rz(-90)
 
 
 def tool_stand_detect() -> np.ndarray | None:
@@ -36,20 +36,14 @@ def tool_stand_detect() -> np.ndarray | None:
         pipeline, intrinsics, TOOL_STAND_TAG_ID, TOOL_STAND_TAG_SIZE_M, detector
     )
     if transform is not None:
-        transform = T_APRIL_TAG_TO_EE_POS @ transform
-        transform = transform @ T_APRIL_TAG_TO_EE_ROT
+        transform = T_APRIL_TAG_TO_EE_POS @ transform @ T_APRIL_TAG_TO_EE_ROT
 
-        # Fix Y (det=-1 mapping can't be done with pure rotations)
-        transform[1, 3] *= -1  # invert Y translation
+        # Fix axis inversions
+        transform[0, 3] *= -1
+        transform[2, 3] *= -1
 
-        # Invert pitch (Y rotation)
-        from scipy.spatial.transform import Rotation
-
-        yaw, pitch, roll = Rotation.from_matrix(transform[:3, :3]).as_euler(
-            "ZYX"
-        )
-        transform[:3, :3] = Rotation.from_euler(
-            "ZYX", [yaw, -pitch, roll]
-        ).as_matrix()
+        # Fix rotation inversions
+        d = np.diag([-1.0, 1.0, -1.0])
+        transform[:3, :3] = d @ transform[:3, :3] @ d
 
     return transform
