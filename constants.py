eec05a61ff2d9a06333c86588797b7d1acc6130c
danielljeon <48095779+danielljeon@users.noreply.github.com>
2026-04-06 18:47:04 -0400

    Add WIP smoothed target pathing

--- constants.py
+++ constants.py
@@ -38,7 +38,3 @@ OPTIMAL_POSE = JointPose([0, 0, -0.17, -3.31, 0, 2.27])
 
 """April tag related constants"""
 APRIL_TAG_SIZE_M_STANDARD = 0.04  # 40 mm for all april tags used.
-
-"""SCREW DRIVER STAND"""
-APRIl_TAG_ID_SCREWDRIVER_STAND = 1
-SCREWDRIVER_STAND_CLEARANCE_HEIGHT_M = 0.1  # Recommended vertical clearance.
