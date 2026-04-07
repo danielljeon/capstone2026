3e0c07aa837a670336f4df24ef7d1a6df3363f82
danielljeon <48095779+danielljeon@users.noreply.github.com>
2026-04-06 21:49:46 -0400

    Abstract tasks and refactor renames

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import numpy as np
-
 from constants import *
 from drivers.motor_rsbl120 import rsbl120_read_position_rad
 from drivers.motor_st3215 import st3215_read_position_rad
+from main_tasks.task_tool_change_screwdriver import tool_change_to_screw_driver
 from robot.end_effectors import (
     EE1_TC,
     EE2_TC,
@@ -11,31 +10,12 @@ from robot.end_effectors import (
 )
 from robot.motor_joints import JOINTS
 from robot_arm import *
-from setup import deinit_comms, set_comms
-from vision import tool_stand_detect
+from setup import confirm_keys, set_comms, deinit_comms
 
 # Environment variables load.
 load_dotenv()  # Load variables from .env.
 URDF_BASE_LINK = os.getenv("URDF_BASE_LINK", "base")
 URDF_PATH = os.getenv("URDF_PATH", "./urdf/robot.urdf")
-
-"""SCREW DRIVER STAND"""
-APRIl_TAG_ID_SCREWDRIVER_STAND = 1
-SCREWDRIVER_STAND_CLEARANCE_HEIGHT_M = 0.075  # Recommended vertical clearance.
-
-
-def confirm_keys(task: str | None = None):
-    while True:
-        prompt = (
-            f"Type 'yes' to continue to {task}: "
-            if task is not None
-            else "Type 'yes' to continue"
-        )
-        resp = input(prompt).strip().lower()
-        if resp == "yes":
-            break
-        print("Please type 'yes' to continue.")
-    print("Continuing...")
 
 
 def pre_run():
@@ -101,71 +81,12 @@ def __go_to_optimal():
     )
 
 
-def __go_to_target_height_offset(april_tag_id: int, height: float):
-    initial_q = [
-        st3215_read_position_rad(JOINTS[0]),
-        rsbl120_read_position_rad(JOINTS[1]),
-        rsbl120_read_position_rad(JOINTS[2]),
-        rsbl120_read_position_rad(JOINTS[3]),
-        rsbl120_read_position_rad(JOINTS[4]),
-        st3215_read_position_rad(JOINTS[5]),
-    ]
-    t_april_tag = tool_stand_detect(april_tag_id, APRIL_TAG_SIZE_M_STANDARD)
-    offset = np.array(
-        [
-            [1, 0, 0, 0],
-            [0, 1, 0, 0],
-            [0, 0, 1, height],
-            [0, 0, 0, 1],
-        ]
-    )
-    q_frames = ik_relative_from_q(
-        urdf_base_link=URDF_BASE_LINK,
-        urdf_path=URDF_PATH,
-        initial_q=initial_q,
-        t_target=t_april_tag,
-        dt=IK_DT_S,
-        min_segment_time=4.0,
-        step_m=0.01,
-        smooth_alpha=0.3,
-        offset=offset,
-    )
-    execute_q_frames(
-        q_frames,
-        JOINTS,
-        dt=IK_DT_S,
-        move_time_ms=int(IK_DT_S * 1000),
-        settle_ms=50,
-    )
-
-
 def run():
     confirm_keys("RELEASE TOOL AND MOVE TO OPTIMAL POSE")
     release_tool_changer(EE2_TC)
     __go_to_optimal()
 
-    confirm_keys("MOVE ABOVE TO <SCREW DRIVER>")
-    for _ in range(3):
-        __go_to_target_height_offset(
-            april_tag_id=APRIl_TAG_ID_SCREWDRIVER_STAND,
-            height=SCREWDRIVER_STAND_CLEARANCE_HEIGHT_M,
-        )
-
-    confirm_keys("MOVE DOWN TO <SCREW DRIVER>")
-    steps = 5
-    height = SCREWDRIVER_STAND_CLEARANCE_HEIGHT_M / steps
-    for i in range(steps):
-        __go_to_target_height_offset(
-            april_tag_id=APRIl_TAG_ID_SCREWDRIVER_STAND,
-            height=height * (steps - (i + 1)),
-        )
-
-    confirm_keys("LOCK TOOL AND MOVE ABOVE <SCREW DRIVER>")
-    lock_tool_changer(EE2_TC)
-    __go_to_target_height_offset(
-        april_tag_id=APRIl_TAG_ID_SCREWDRIVER_STAND,
-        height=SCREWDRIVER_STAND_CLEARANCE_HEIGHT_M,
-    )
+    tool_change_to_screw_driver()
 
 
 if __name__ == "__main__":
---
+++ main_tasks/task_tool_change_screwdriver.py
@@ -0,0 +1,36 @@
+from .abstracted import go_to_target_height_offset
+from robot.end_effectors import (
+    EE2_TC,
+    lock_tool_changer,
+)
+from setup import confirm_keys
+
+"""SCREW DRIVER STAND"""
+APRIl_TAG_ID_SCREWDRIVER_STAND = 1
+SCREWDRIVER_STAND_M = 0.075  # Tool stand entry vertical clearance.
+SCREWDRIVER_STAND_AND_TOOL_M = 0.1  # Tool stand exit and raise clearance.
+
+
+def tool_change_to_screw_driver():
+    confirm_keys("MOVE ABOVE TO <SCREW DRIVER>")
+    for _ in range(3):
+        go_to_target_height_offset(
+            april_tag_id=APRIl_TAG_ID_SCREWDRIVER_STAND,
+            height=SCREWDRIVER_STAND_M,
+        )
+
+    confirm_keys("MOVE DOWN TO <SCREW DRIVER>")
+    steps = 5
+    height = SCREWDRIVER_STAND_M / steps
+    for i in range(steps):
+        go_to_target_height_offset(
+            april_tag_id=APRIl_TAG_ID_SCREWDRIVER_STAND,
+            height=height * (steps - (i + 1)),
+        )
+
+    confirm_keys("LOCK TOOL AND MOVE ABOVE <SCREW DRIVER>")
+    lock_tool_changer(EE2_TC)
+    go_to_target_height_offset(
+        april_tag_id=APRIl_TAG_ID_SCREWDRIVER_STAND,
+        height=SCREWDRIVER_STAND_AND_TOOL_M,
+    )
