453ec1576fe3b353169426f94d3ce128b83532cc
danielljeon <48095779+danielljeon@users.noreply.github.com>
2026-04-06 21:55:51 -0400

    Parameterize min_segment_time in abstracted motion

--- main_tasks/task_tool_change_screwdriver.py
+++ main_tasks/task_tool_change_screwdriver.py
@@ -1,14 +1,14 @@
-from .abstracted import go_to_target_height_offset
 from robot.end_effectors import (
     EE2_TC,
     lock_tool_changer,
 )
 from setup import confirm_keys
+from .abstracted import go_to_target_height_offset
 
 """SCREW DRIVER STAND"""
 APRIl_TAG_ID_SCREWDRIVER_STAND = 1
 SCREWDRIVER_STAND_M = 0.075  # Tool stand entry vertical clearance.
-SCREWDRIVER_STAND_AND_TOOL_M = 0.1  # Tool stand exit and raise clearance.
+SCREWDRIVER_STAND_AND_TOOL_M = 0.15  # Tool stand exit and raise clearance.
 
 
 def tool_change_to_screw_driver():
@@ -26,6 +26,7 @@ def tool_change_to_screw_driver():
         go_to_target_height_offset(
             april_tag_id=APRIl_TAG_ID_SCREWDRIVER_STAND,
             height=height * (steps - (i + 1)),
+            min_segment_time=1.0,  # Smaller segment times.
         )
 
     confirm_keys("LOCK TOOL AND MOVE ABOVE <SCREW DRIVER>")
