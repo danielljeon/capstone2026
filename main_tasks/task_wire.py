2665d9e175c350ac4848e6142f37271bd703f507
danielljeon <48095779+danielljeon@users.noreply.github.com>
2026-04-13 03:46:52 -0400

    Add working wire task

--- main_tasks/task_wire.py
+++ main_tasks/task_wire.py
@@ -1,20 +1,17 @@
 import time
 
-import numpy as np
-
 from constants import TOOL_LOG, TOOL_LOG_PLOT
 from robot.end_effectors import (
     EE2_TOOL,
     run_tool_end,
 )
 from setup import confirm_keys
-from .abstracted import go_to_target_height_offset, go_to_target_offset
+from .abstracted import go_to_target_height_offset
 
 """WIRE TASK"""
 APRIL_TAG_ID_WIRE_TASK = 4
-APRIL_TAG_SIZE_M_WIRE_TASK = 0.04
-WIRE_TARGET_VERTICAL_M = 0.08  # Vertical clearance.
-WIRE_TARGET_LATERAL_M = 0.05  # Lateral clearance.
+APRIL_TAG_SIZE_M_WIRE_TASK = 0.03
+WIRE_TARGET_VERTICAL_M = 0.075  # Vertical clearance.
 WIRE_TASK_CALIBRATION_FILE_PATH = (
     "computer_vision_cals/april_tag_cal_wire_task.csv"
 )
@@ -26,11 +23,13 @@ def wire_replug(safety_on: bool = True):
     else:
         time.sleep(1)
 
-    for _ in range(3):
+    for i in range(3):
         go_to_target_height_offset(
             april_tag_id=APRIL_TAG_ID_WIRE_TASK,
             april_tag_size_m=APRIL_TAG_SIZE_M_WIRE_TASK,
             height=WIRE_TARGET_VERTICAL_M,
+            # Lock orientation only when close.
+            lock_full_orientation=True if i > 0 else False,
             april_tag_calibration_filepath=WIRE_TASK_CALIBRATION_FILE_PATH,
         )
 
@@ -39,7 +38,7 @@ def wire_replug(safety_on: bool = True):
     else:
         time.sleep(1)
 
-    steps = 5
+    steps = 3
     height = WIRE_TARGET_VERTICAL_M / steps
     for i in range(steps):
         go_to_target_height_offset(
@@ -58,10 +57,10 @@ def wire_replug(safety_on: bool = True):
         hbridge=EE2_TOOL,
         speed=1,
         duration_s=10,
-        reverse=False,
-        current_limit=350,
+        reverse=True,
+        current_limit=450,
         ignore_start_current=True,
-        start_current_time_s=0.5,
+        start_current_time_s=2.0,
         plot_playback=TOOL_LOG_PLOT,
         playback_csv_dir=("." if TOOL_LOG else None),
     )
@@ -71,40 +70,40 @@ def wire_replug(safety_on: bool = True):
     else:
         time.sleep(1)
 
-    offset = np.array(
-        [
-            [1, 0, 0, WIRE_TARGET_LATERAL_M],
-            [0, 1, 0, 0],
-            [0, 0, 1, 0],
-            [0, 0, 0, 1],
-        ]
-    )
-    go_to_target_offset(
+    go_to_target_height_offset(
         april_tag_id=APRIL_TAG_ID_WIRE_TASK,
         april_tag_size_m=APRIL_TAG_SIZE_M_WIRE_TASK,
-        offset=offset,
+        height=WIRE_TARGET_VERTICAL_M,
         april_tag_calibration_filepath=WIRE_TASK_CALIBRATION_FILE_PATH,
     )
 
     if safety_on:
-        confirm_keys("REPLUG WIRE <WIRE>")
+        confirm_keys("MOVE ABOVE AND ALIGN <WIRE>")
     else:
         time.sleep(1)
 
-    offset = np.array(
-        [
-            [1, 0, 0, 0],
-            [0, 1, 0, 0],
-            [0, 0, 1, 0],
-            [0, 0, 0, 1],
-        ]
-    )
-    go_to_target_offset(
-        april_tag_id=APRIL_TAG_ID_WIRE_TASK,
-        april_tag_size_m=APRIL_TAG_SIZE_M_WIRE_TASK,
-        offset=offset,
-        april_tag_calibration_filepath=WIRE_TASK_CALIBRATION_FILE_PATH,
-    )
+    for _ in range(3):
+        go_to_target_height_offset(
+            april_tag_id=APRIL_TAG_ID_WIRE_TASK,
+            april_tag_size_m=APRIL_TAG_SIZE_M_WIRE_TASK,
+            height=WIRE_TARGET_VERTICAL_M,
+            april_tag_calibration_filepath=WIRE_TASK_CALIBRATION_FILE_PATH,
+        )
+
+    if safety_on:
+        confirm_keys("MOVE DOWN TO <WIRE>")
+    else:
+        time.sleep(1)
+
+    steps = 3
+    height = WIRE_TARGET_VERTICAL_M / steps
+    for i in range(steps):
+        go_to_target_height_offset(
+            april_tag_id=APRIL_TAG_ID_WIRE_TASK,
+            april_tag_size_m=APRIL_TAG_SIZE_M_WIRE_TASK,
+            height=height * (steps - (i + 1)),
+            april_tag_calibration_filepath=WIRE_TASK_CALIBRATION_FILE_PATH,
+        )
 
     if safety_on:
         confirm_keys("OPEN CLAW <WIRE>")
@@ -115,10 +114,10 @@ def wire_replug(safety_on: bool = True):
         hbridge=EE2_TOOL,
         speed=1,
         duration_s=10,
-        reverse=True,
-        current_limit=300,
+        reverse=False,
+        current_limit=450,
         ignore_start_current=True,
-        start_current_time_s=1.0,
+        start_current_time_s=1.5,
         plot_playback=TOOL_LOG_PLOT,
         playback_csv_dir=("." if TOOL_LOG else None),
     )
